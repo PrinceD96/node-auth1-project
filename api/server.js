79c5ddbc02e7ff2349c5f85fe597db85b0f9f95f
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-04-22 09:51:15 -0400

    Implemented sessions. Wrote GET request for login

--- api/server.js
+++ api/server.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const helmet = require("helmet");
+const session = require("express-session");
 
 const usersRouter = require("./routes/users/usersRouter");
+const authRouter = require("../auth/authRouter");
 
 const server = express();
 
+const sessionConfig = {
+	name: "unknown",
+	secret: "If I knew I would've told you already",
+	cookie: {
+		maxAge: 1000 * 30,
+		secure: false,
+		httpOnly: true
+	},
+	resave: false,
+	saveUninitialized: false
+};
+
 server.use(express.json());
 server.use(helmet());
+server.use(session(sessionConfig));
 server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
 	res.json({ api: "up" });
--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,3 +1,40 @@
 const bcrypt = require("bcryptjs");
 
 const router = require("express").Router();
+const Users = require("../api/routes/users/usersModel");
+const { validateUser } = require("../api/routes/users/usersMiddleware");
+
+router.post("/register", validateUser, (req, res) => {
+	let user = req.body;
+
+	const hash = bcrypt.hashSync(user.password, 12);
+
+	user.password = hash;
+
+	Users.add(user)
+		.then(newUser => {
+			res.status(201).json({ newUser });
+		})
+		.catch(error => {
+			res.status(500).json({ error: "Problem with the database", error });
+		});
+});
+
+router.post("/login", validateUser, (req, res) => {
+	const { username, password } = req.body;
+
+	Users.findBy({ username })
+		.then(([user]) => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				req.session.user = username;
+				res.status(200).json({ message: "Welcome" });
+			} else {
+				res.status(401).json({ message: "Invalid credentials" });
+			}
+		})
+		.catch(error =>
+			res.status(500).json({ error: "Problem with the database", error })
+		);
+});
+
+module.exports = router;
