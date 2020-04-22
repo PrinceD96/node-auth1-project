6735162e9abc4f005d2c0f66a554d6f661ad553c
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-04-22 01:35:04 -0400

    knex initial boilerplate. Created users table

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ const helmet = require("helmet");
 
 const server = express();
 
+server.use(express.json());
+server.use(helmet());
+
 server.get("/", (req, res) => {
 	res.json({ api: "up" });
 });
